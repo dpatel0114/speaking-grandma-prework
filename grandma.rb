0af90f8afbec2db3d055eefcd3a1c2fde5527ea3
Dhara Patel <1991dharapatel@gmail.com>
2019-03-16 17:41:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase.upcase==phrase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == 
+  elsif phrase == "I LOVE YOU GRANDMA!"
     
   else
     return "HUH?! SPEAK UP, SONNY!"
