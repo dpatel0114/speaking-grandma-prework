918bb6a090a0edab1918be5afcd3e0c0cca97bcc
Dhara Patel <1991dharapatel@gmail.com>
2019-03-16 17:46:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  if phrase.upcase==phrase
+  elsif phrase.upcase==phrase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+
   else
     return "HUH?! SPEAK UP, SONNY!"
  end
